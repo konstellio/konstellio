5be83f4baa369d53166007aa0c905a5d5cd8b520
MIchael Grenier <michael.grenier@live.com>
2017-11-16 13:40:13 -0500

    Build schema from definition

---
+++ packages/server/src/schemas.ts
@@ -0,0 +1,125 @@
+
+export type FieldResolverFunction = (parent: any, args: any, context: any, info: any) => any | Promise<any>;
+export type FieldResolverFragment = {
+	fragment: string
+	resolve: (parent: any, args: any, context: any, info: any) => any | Promise<any>
+}
+export type FieldResolver = {
+	[key: string]: FieldResolverFunction | FieldResolverFragment
+}
+export type Resolvers = {
+	[key: string]: FieldResolver
+};
+export type Schema = {
+	typeDefs: string
+	resolvers?: Resolvers
+}
+
+const schemas: Schema[] = [{
+	typeDefs: `
+		scalar Cursor
+		scalar Date
+		scalar DateTime
+
+		type User @model {
+			id: ID!
+			username: String! @field(label: "Username", type: "text")
+			password: String! @field(label: "Password", type: "password") @permission(group: "noone")
+		}
+
+		type File @model {
+			id: ID!
+			path: String! @field(type: "text")
+			name: String! @field(type: "text")
+			size: Int! @field(type: "int")
+			creation: DateTime! @field(type: "datetime")
+			modification: DateTime! @field(type: "datetime")
+		}
+
+		type Query {
+			whoami: User!
+		}
+
+		type LoginResponse {
+			token: String!
+		}
+
+		type LogoutResponse {
+			acknowledge: Boolean!
+		}
+
+		type Mutation {
+			login(username: String!, password: String!): LoginResponse @permission(group: "nobody")
+			logout: LogoutResponse @permission(group: "any")
+		}
+	`,
+	resolvers: {
+		Query: {
+			async whoami() {
+				return {
+					id: 'needs-an-ID',
+					username: 'someone'
+				}
+			}
+		},
+		Mutation: {
+			async login(parent, { username, password }, context, info) {
+				return {
+					token: "what-token?"
+				};
+			},
+			async logout() {
+				return {
+					acknowledge: true
+				};
+			}
+		}
+	}
+}, {
+	typeDefs: `
+		extend type User {
+			birthday: Date! @field(label: "Birthday", type: "date")
+			displayName: String!
+		}
+	`,
+	resolvers: {
+		User: {
+			displayName: {
+				fragment: `fragment UserFragment on User { username }`,
+				async resolve(parent, args, context, info) {
+					return parent.username;
+				}
+			}
+		}
+	}
+}, {
+	typeDefs: `
+		type Post @model {
+			id: ID!
+			title: String! @field(label: "Title", type: "text")
+			slug: String! @field(type: "slug", on: "title")
+			postDate: DateTime! @field(label: "Post date", type: "datetime")
+			expireDate: DateTime @field(label: "Expire date", type: "datetime")
+			author: User! @field(label: "Author", type: "relation")
+			content: String! @field(label: "Content", type: "html")
+		}
+
+		type PostCursor {
+			cursor: Cursor
+			item: Post!
+		}
+
+		extend type Query {
+			latestPost(first: Int, after: Cursor): [PostCursor!]!
+		}
+	`,
+	resolvers: {
+		Query: {
+			async latestPost(parent, { first, after }, context, info) {
+				return [];
+			}
+		}
+	}
+}];
+
+export default schemas;
--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,33 +1,90 @@
+import { createServer } from 'http';
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import { parse } from 'graphql';
+import { buildSchemaFromTypeDefinitions, addResolveFunctionsToSchema } from 'graphql-tools';
 import { q, SQLiteDriver } from 'konstellio-db';
 
+import schemas from './schemas';
+// TODO should get schema from DB (both definition & resolvers code)
+
+// Merge definition & resolvers
+const typeDefs = schemas.reduce((acc, schema) => {
+    return acc + `\n` + schema.typeDefs;
+}, '');
+const resolvers = schemas.reduce((acc, schema) => {
+    const resolvers = schema.resolvers || {};
+    Object.keys(resolvers).forEach(key => {
+        acc[key] = acc[key] || {};
+        Object.assign(acc[key], resolvers[key]);
+    });
+    return acc;
+}, {});
+
+const ast = parse(typeDefs);
+// TODO parse `ast` for DB model directive @model, @field
+
 const app = express();
-const db = new SQLiteDriver({
-    filename: 'F:\\Apps\\konstellio\\sculptor-project\\sculptor.sqlite'
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+app.use('/graphql', graphqlExpress((req) => {
+    // TODO clone/copy `ast` per "group"
+    // TODO modify `ast` with AST transformer (hide @permission, etc)
+    // TODO cache the new `ast` for "group"
+    
+    // Example : remove type/field with @hidden directive
+    // function visible (node): boolean {
+    //     return node.directives != null && !node.directives.some(directive => directive.name.value === 'hidden');
+    // }
+    // ast.definitions = ast.definitions.filter(visible);
+    // ast.definitions.forEach((definition: any) => {
+    //     if (definition.fields) {
+    //         definition.fields = definition.fields.filter(visible);
+    //     }
+    // })
+
+    const schema = buildSchemaFromTypeDefinitions(ast);
+    addResolveFunctionsToSchema(schema, resolvers);
+
+    return {
+        schema
+    };
+}));
+
+const server = createServer(app);
+server.listen(8080, () => {
+    console.log(`GraphQL server is now running on http://localhost:8080/.`);
 });
 
-db.connect();
 
-interface page {
-    id: number
-    parent?: number
-    seq: number
-    lft: number
-    rgt: number
-}
+// const db = new SQLiteDriver({
+//     filename: 'F:\\Apps\\konstellio\\sculptor-project\\sculptor.sqlite'
+// });
 
-app.get('/', async (req, res) => {
-    res.set('Content-Type', 'text/plain');
-    try {
-        let entry = await db.execute<page>(q.select().from('entry_page').eq('id', 2));
-        let page = entry.results[0];
-        let children = await db.execute<page>(q.select().from('entry_page').where(q.and(q.gte('lft', page.lft), q.lte('rgt', page.rgt))));
-        res.send(`${JSON.stringify(children.results)}`);
-    } catch (err) {
-        res.send(`${err.stack}`);
-    }
-});
+// db.connect();
 
-app.listen(8080, () => {
-    console.log(`Server listening to ...`);
-});
+// interface page {
+//     id: number
+//     parent?: number
+//     seq: number
+//     lft: number
+//     rgt: number
+// }
+
+// app.get('/', async (req, res) => {
+//     res.set('Content-Type', 'text/plain');
+//     try {
+//         let entry = await db.execute<page>(q.select().from('entry_page').eq('id', 2));
+//         let page = entry.results[0];
+//         let children = await db.execute<page>(q.select().from('entry_page').where(q.and(q.gte('lft', page.lft), q.lte('rgt', page.rgt))));
+//         res.send(`${JSON.stringify(children.results)}`);
+//     } catch (err) {
+//         res.send(`${err.stack}`);
+//     }
+// });
+
+// app.listen(8080, () => {
+//     console.log(`Server listening to ...`);
+// });
