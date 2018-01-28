1485d93110ba6228e29e010112b74896aef6dda4
Michael Grenier <michael.grenier@live.com>
2018-01-28 13:32:12 -0500

    ...

--- packages/mq/src/Drivers/MemoryDriver.spec.ts
+++ packages/mq/src/Drivers/MemoryDriver.spec.ts
@@ -2,37 +2,36 @@ import 'mocha';
 import { use, expect, should } from 'chai';
 use(require("chai-as-promised"));
 should();
-import { spawn, ChildProcess } from 'child_process';
 import { MemoryDriver } from './MemoryDriver';
 import { setTimeout } from 'timers';
 
 describe('RedisMock', () => {
 
-	const driver: MemoryDriver = new MemoryDriver();
+	const mq: MemoryDriver = new MemoryDriver();
 
 	before(done => {
-		driver.connect().then(() => done());
+		mq.connect().then(() => done());
 	});
 	
 	it('test', async () => {
 		
-		const ch = await driver.createChannel('test1');
+		const ch = await mq.createChannel('test1');
 		ch.subscribe(msg => {
-			console.log('Subscribe', msg.ts, msg.sender, msg.content.toString('utf8'));
+			console.log('Subscribe', msg.ts, msg.sender, msg.content.toString());
 		});
 		// setInterval(() => {
 		// 	ch.publish(Buffer.from('Yay'));
 		// }, 200);
 
-		const qu = await driver.createQueue('test2');
+		const qu = await mq.createQueue('test2');
 		qu.consume(async (msg) => {
 			await new Promise(resolve => setTimeout(resolve, 2000));
-			console.log('Consume', msg.ts, msg.sender, msg.content.toString('utf8'));
+			console.log('Consume', msg.ts, msg.sender, msg.content.toString());
 			return msg.content;
 		});
 		qu.send(Buffer.from('Yay1'))
 		qu.await(Buffer.from('Yay2')).then(msg => {
-			console.log('Done', msg.ts, msg.sender, msg.content.toString('utf8'));
+			console.log('Done', msg.ts, msg.sender, msg.content.toString());
 		});
 
 	})
