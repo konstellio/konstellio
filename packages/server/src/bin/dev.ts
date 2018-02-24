405398a26d1f279aac0fc122e70c31b808075e7e
Michael Grenier <michael.grenier@live.com>
2018-02-24 14:39:52 -0500

    More progress on models

--- packages/server/src/bin/dev.ts
+++ packages/server/src/bin/dev.ts
@@ -6,7 +6,8 @@ import { getSchemaDocument, getSchemaResolvers, parseSchema } from '../utils/sch
 import { dirname } from 'path';
 import { getSchemaDiff, executeSchemaMigration } from '../utils/migration';
 import { ReadStream, WriteStream } from 'tty';
-import { getModels } from '../utils/model';
+import { getModels, Model } from '../utils/model';
+import { q } from '@konstellio/db';
 
 export default async function ({ file }) {
 	const config = await parseConfig(file);
@@ -51,6 +52,63 @@ export default async function ({ file }) {
 	}
 
 	const models = await getModels(context, schemas);
+
+	type User = {
+		username: string
+	}
+	type UserInput = {
+		username: string
+		password: string
+	}
+
+	type Post = {
+		title: string
+		slug: string
+		postDate: Date
+		expireDate?: Date
+		content: string
+	}
+	type PostInput = {
+		title: {
+			fr: string
+			en: string
+		}
+		slug: {
+			fr: string
+			en: string
+		}
+		postDate: Date
+		expireDate?: Date
+		content: {
+			fr: string
+			en: string
+		}
+	}
+
+	const user = models.get('User')! as Model<User, UserInput>;
+	const post = models.get('Post')! as Model<Post, PostInput>;
+
+	// const u = await user.create({ username: 'lpaudet', password: 'bleh' });
+	// const u = await user.findOne({ condition: q.eq('id', '5a90c8373d116f05dca8fc5c') });
+	// const u = await user.findById('5a90c8373d116f05dca8fc5c');
+	// console.log(JSON.stringify(u));
+
+	// const us = await user.findByIds(['5a90c8373d116f05dca8fc5c', '5a91ab4b52ff84191c1ae33c'], { fields: ['username'] });
+	// const us = await Promise.all([
+	// 	user.findById('5a90c8373d116f05dca8fc5c'),
+	// 	user.findById('5a90c8373d116f05dca8fc5c', { fields: ['username'] }),
+	// 	user.findById('5a91ab4b52ff84191c1ae33c', { fields: ['id'] })
+	// ])
+	// const us = await user.find();
+
+	const p = await post.create({
+		title: { fr: 'Premier post', en: 'First post' },
+		slug: { fr: 'premier-post', en: 'first-post' },
+		postDate: new Date(),
+		content: { fr: '...', en: '...' }
+	});
+
+	const ps = await post.find({ locale: 'fr' });
 
 	debugger;
 }
--- packages/server/src/utils/model.ts
+++ packages/server/src/utils/model.ts
@@ -1,4 +1,4 @@
-import { Driver, q, Collection, Bitwise, SortableField, CalcField, FieldExpression, ValueExpression, Comparison, SelectQuery, Expression } from "@konstellio/db";
+import { Driver, q, Collection, Bitwise, SortableField, CalcField, FieldExpression, ValueExpression, Comparison, SelectQuery, Expression, Query } from "@konstellio/db";
 import { Schema } from "./schema";
 import { PluginInitContext } from "./plugin";
 import ObjectID from "bson-objectid"
@@ -16,9 +16,15 @@ export async function getModels(context: PluginInitContext, schemas: Schema[]): 
 	return models;
 }
 
-export class Model<T extends { id: string, [field: string]: ValueExpression } = { id: string }> {
+export type ModelType = { [field: string]: undefined | ValueExpression };
+export type ModelInputType = { [field: string]: undefined | ValueExpression | { [locale: string]: ValueExpression } };
+
+export type WithID<O> = {[K in keyof O]: O[K]} & { id: string };
+
+export class Model<O extends ModelType = {}, I extends ModelInputType = {}> {
 	
-	protected readonly collection: Collection;
+	private readonly collection: Collection;
+	private readonly loader: Dataloader<string, WithID<O> | undefined>;
 
 	constructor(
 		protected readonly database: Driver,
@@ -26,9 +32,19 @@ export class Model<T extends { id: string, [field: string]: ValueExpression } = 
 		protected readonly models: Map<string, Model>
 	) {
 		this.collection = q.collection(schema.handle);
+		this.loader = new Dataloader<string, WithID<O> | undefined>(
+			async (keys) => {
+				// TODO somehow accumulate fields to only fetch fields that are commons across `keys`
+				const query = q.select().from(this.collection).in('id', keys);
+				const result = await this.database.execute<WithID<O>>(query);
+				
+				return keys.map(id => {
+					const res = result.results.filter(result => result.id === id);
+					return res.length === 1 ? res[0] : undefined;
+				});
+			}
+		)
 	}
-
-	// TODO "localize" results and queries
 
 	async findById(
 		id: string,
@@ -36,20 +52,18 @@ export class Model<T extends { id: string, [field: string]: ValueExpression } = 
 			fields?: string[]
 			locale?: string
 		}
-	): Promise<T> {
-		// TODO Dataloader.load https://github.com/facebook/dataloader#loadkey
-		let query = q.select().from(this.collection).eq('id', id).limit(1);
-
-		if (options && options.fields) {
-			query = query.select(...options.fields);
-		}
-
-		const result = await this.database.execute<T>(query);
-		if (result.results.length === 0) {
-			throw new Error(`Could not find id ${id} in ${this.schema.handle}.`);
-		}
-
-		return result.results[0];
+	): Promise<WithID<O>> {
+		try {
+			const result = await this.loader.load(id);
+			if (result) {
+				return this.reduceResult<WithID<O>>(
+					result,
+					options && options.locale,
+					options && options.fields && ['id'].concat(options.fields.filter(key => key !== 'id'))
+				) as WithID<O>;
+			}
+		} catch (err) { }
+		throw new Error(`Could not find ID ${id} in ${this.schema.handle}.`);
 	}
 
 	async findByIds(
@@ -58,18 +72,19 @@ export class Model<T extends { id: string, [field: string]: ValueExpression } = 
 			fields?: string[]
 			locale?: string
 		}
-	): Promise<T[]> {
-		// TODO Dataloader.loadMany https://github.com/facebook/dataloader#loadmanykeys
-
-		let query = q.select().from(this.collection).in('id', ids);
-
-		if (options && options.fields) {
-			query = query.select(...options.fields);
-		}
-
-		const result = await this.database.execute<T>(query);
-
-		return result.results;
+	): Promise<WithID<O>[]> {
+		try {
+			const results = await this.loader.loadMany(ids);
+			const realResults = results.filter((result): result is WithID<O> => result !== undefined);
+			if (realResults.length === ids.length) {
+				return realResults.map((result: WithID<O>) => this.reduceResult<WithID<O>>(
+					result,
+					options && options.locale,
+					options && options.fields && ['id'].concat(options.fields.filter(key => key !== 'id')))
+				) as WithID<O>[];
+			}
+		} catch (err) {}
+		throw new Error(`Could not find IDs ${ids.join(', ')} in ${this.schema.handle}.`);
 	}
 
 	async findOne(options?: {
@@ -78,12 +93,12 @@ export class Model<T extends { id: string, [field: string]: ValueExpression } = 
 		condition?: Bitwise | Comparison
 		sort?: SortableField[]
 		offset?: number
-	}): Promise<T> {
+	}): Promise<WithID<O>> {
 		let query = q.select().from(this.collection).limit(1);
 
 		if (options) {
 			if (options.fields) {
-				query = query.select(...options.fields);
+				query = query.select(...['id'].concat(options.fields.filter(key => key !== 'id')));
 			}
 			if (options.condition) {
 				query = query.where(options.condition instanceof Bitwise ? options.condition : q.and(options.condition));
@@ -96,12 +111,12 @@ export class Model<T extends { id: string, [field: string]: ValueExpression } = 
 			}
 		}
 
-		const result = await this.database.execute<T>(query);
+		const result = await this.database.execute<WithID<O>>(query);
 		if (result.results.length === 0) {
 			throw new Error(`Could not find anything matching query in ${this.schema.handle}.`);
 		}
 
-		return result.results[0];
+		return this.reduceResult<WithID<O>>(result.results[0], options && options.locale);
 	}
 
 	async find(options?: {
@@ -111,12 +126,12 @@ export class Model<T extends { id: string, [field: string]: ValueExpression } = 
 		sort?: SortableField[]
 		offset?: number
 		limit?: number
-	}): Promise<T[]> {
+	}): Promise<WithID<O>[]> {
 		let query = q.select().from(this.collection);
 
 		if (options) {
 			if (options.fields) {
-				query = query.select(...options.fields);
+				query = query.select(...['id'].concat(options.fields.filter(key => key !== 'id')));
 			}
 			if (options.condition) {
 				query = query.where(options.condition instanceof Bitwise ? options.condition : q.and(options.condition));
@@ -132,12 +147,12 @@ export class Model<T extends { id: string, [field: string]: ValueExpression } = 
 			}
 		}
 
-		const result = await this.database.execute<T>(query);
+		const result = await this.database.execute<WithID<O>>(query);
 
-		return result.results;
+		return result.results.map(result => this.reduceResult<WithID<O>>(result, options && options.locale));
 	}
 
-	async aggregate<R>(options?: {
+	async aggregate<O>(options?: {
 		locale?: string
 		fields?: (string | CalcField)[]
 		joins?: { [alias: string]: { query: SelectQuery, on: Expression } }
@@ -146,41 +161,88 @@ export class Model<T extends { id: string, [field: string]: ValueExpression } = 
 		sort?: SortableField[]
 		offset?: number
 		limit?: number
-	}): Promise<R[]> {
+	}): Promise<O[]> {
+		// TODO localized fields, condition, group, sort and results
 		throw new Error(`Model.aggregate not implemented yet.`);
 	}
 
-	async create(data: T, locale?: string): Promise<T> {
+	async create(data: I): Promise<string> {
 		const id = ObjectID.generate();
-		const obj: T = Object.assign({}, data, { id });
+		const obj: WithID<I> = Object.assign({}, data, { id });
 
+		// TODO localized fields
+		debugger;
+		
 		const query = q.insert(this.collection.name, this.collection.namespace).fields(obj);
 		const result = await this.database.execute(query);
 
-		return obj;
+		return id;
 	}
 
-	async update(id: string, data: T, locale?: string): Promise<T> {
-		throw new Error(`Model.update not implemented yet.`);
+	async update(data: WithID<I>): Promise<string> {
+		if (('id' in data) === false) {
+			throw new Error(`Could not retrieve property id of data.`);
+		}
+
+		const { id, ...obj } = data as any;
+
+		// TODO localized fields
+		debugger;
+
+		const query = q.update(this.collection.name, this.collection.namespace).fields(data);
+		const result = await this.database.execute(query);
+
+		return id;
 	}
 
 	async delete(...ids: string[]): Promise<boolean> {
-		// TODO Dataloader.clear https://github.com/facebook/dataloader#clearkey
-		throw new Error(`Model.delete not implemented yet.`);
+		const query = q.delete(this.collection.name, this.collection.namespace).in('id', ids);
+		const result = await this.database.execute(query);
+		ids.forEach(id => this.loader.clear(id));
+		return result.acknowledge;
 	}
 
-	async relation<R>(
+	async relation<O>(
 		field: string,
 		options: {
 			locale?: string
-			fields?: (string | CalcField)[]
+			fields?: string[]
 			condition?: Bitwise | Comparison
 			offset?: number
 			limit?: number
 		}
-	): Promise<R[]> {
+	): Promise<WithID<O>[]> {
 		// TODO Dataloader.loadMany https://github.com/facebook/dataloader#loadmanykeys
+		// TODO localized fields, condition and results
 		throw new Error(`Model.relation not implemented yet.`);
 	}
 
+	private reduceResult<O>(data: O, locale?: string, fields?: string[]): O {
+		if (locale || fields) {
+			const localized = locale
+				? this.schema.fields.reduce((fields, field) => {
+					if (field.localized === true) {
+						fields.push(field.handle);
+					}
+					return fields;
+				}, [] as string[])
+				: [];
+			
+			return Object.keys(data).reduce((filtered, source) => {
+				let target: string = source;
+				if (localized.indexOf(source) > -1) {
+					target = source.replace(/__([a-z]{2}(-[a-zA-Z]{2})?)$/, '');
+				}
+				if (fields && fields.indexOf(target) > -1) {
+					filtered[target] = data[source];
+				}
+				return filtered;
+			}, {} as O);
+		}
+		return data;
+	}
+
+	private localizeQuery<Q extends Query>(query: Q): Q {
+		return query;
+	}
 }
