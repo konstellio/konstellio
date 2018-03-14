2606283e4c8ec130e5dd12921ad4c1893809bff3
Michael Grenier <michael.grenier@live.com>
2018-03-13 21:10:20 -0400

    First beta of Records

--- packages/server/src/bin/dev.ts
+++ packages/server/src/bin/dev.ts
@@ -6,7 +6,7 @@ import { getSchemaDocument, getSchemaResolvers, parseSchema } from '../utils/sch
 import { dirname } from 'path';
 import { getSchemaDiff, executeSchemaMigration } from '../utils/migration';
 import { ReadStream, WriteStream } from 'tty';
-import { getModels, Model } from '../utils/model';
+import { getRecords, Record } from '../utils/record';
 import { q } from '@konstellio/db';
 
 export default async function ({ file }) {
@@ -51,57 +51,7 @@ export default async function ({ file }) {
 		}
 	}
 
-	const models = await getModels(context, schemas, Object.keys(locales));
-
-	const User = models.get('User')! as Model<
-		{ username: string, password: string },
-		{ id: string, username: string, password: string }
-	>;
-	const Post = models.get('Post')! as Model<
-		{ title: { fr: string, en: string }, slug: { fr: string, en: string }, postDate: Date, expireDate?: Date, author: string[], content: { fr: string, en: string } },
-		{ id: string, title: string, slug: string, postDate: Date, expireDate: Date, author: string[], content: string }
-	>;
-
-	// const data = {
-	// 	title: { fr: 'Premier post', en: 'First post' },
-	// 	slug: { fr: 'premier-post', en: 'first-post' },
-	// 	author: ['5a90c8373d116f05dca8fc5c'],
-	// 	postDate: new Date(),
-	// 	content: { fr: '...', en: '...' }
-	// };
-	// if (Post.validate(data)) {
-	// 	const post = await Post.create(data);
-	// }
-
-	// const data = {
-	// 	id: '5aa1d9965ab8cb158cbe5191',
-	// 	title: { fr: 'Deuxième post', en: 'Second post' },
-	// 	slug: { fr: 'deuxieme-post', en: 'second-post' },
-	// 	author: ['5a90c8373d116f05dca8fc5c'],
-	// 	postDate: new Date(),
-	// 	content: { fr: '...', en: '...' }
-	// }
-	// if (Post.validate(data)) {
-	// 	const post = await Post.replace(data);
-	// }
-
-	// const [a, b] = await Promise.all([
-	// 	Post.findById('5aa1d9965ab8cb158cbe5191', { locale: 'fr', fields: ['id', 'title', 'author'] }),
-	// 	Post.findById('5aa1d9965ab8cb158cbe5191', { locale: 'en', fields: ['postDate'] })
-	// ]);
-
-	// const p = await Post.find({ condition: q.eq('slug', 'premier-post'), locale: 'fr' });
-	const p = await Post.findOne({ condition: q.eq('author', '5a90c8373d116f05dca8fc5c'), locale: 'fr' });
-
-	// const errors = [];
-	// const data = {
-	// 	id: '...',
-	// 	username: 'test',
-	// 	password: '10'
-	// };
-	// if (User.validate(data, errors)) {
-		
-	// }
+	const records = await getRecords(context, schemas, Object.keys(locales));
 
 	debugger;
 }
