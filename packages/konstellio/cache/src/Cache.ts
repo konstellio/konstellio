9dd27a0b3d2185492556884fc31acab81a7dae76
Michael Grenier <michael.grenier@live.com>
2018-09-01 13:50:29 -0400

    Fixed lint & missing test for Cache

--- packages/konstellio/cache/src/Cache.ts
+++ packages/konstellio/cache/src/Cache.ts
@@ -1,16 +1,16 @@
 import { IDisposableAsync } from '@konstellio/disposable';
 
-export type Serializable = string | number | boolean | Date
+export type Serializable = string | number | boolean | Date;
 
 export abstract class Cache implements IDisposableAsync {
 
-	abstract connect(): Promise<this>
-	abstract disconnect(): Promise<void>
-	abstract set(key: string, value: Serializable, ttl: number): Promise<void>
-	abstract expire(key: string, ttl: number): Promise<void>
-	abstract get(key: string): Promise<Serializable>
-	abstract has(key: string): Promise<boolean>
-	abstract unset(key: string): Promise<void>
+	abstract connect(): Promise<this>;
+	abstract disconnect(): Promise<void>;
+	abstract set(key: string, value: Serializable, ttl: number): Promise<void>;
+	abstract expire(key: string, ttl: number): Promise<void>;
+	abstract get(key: string): Promise<Serializable>;
+	abstract has(key: string): Promise<boolean>;
+	abstract unset(key: string): Promise<void>;
 	abstract isDisposed(): boolean;
 	abstract disposeAsync(): Promise<void>;
 
---
+++ packages/konstellio/cache/test/Cache.ts
@@ -0,0 +1,9 @@
+import 'mocha';
+import { use, should } from 'chai';
+use(require("chai-as-promised"));
+should();
+import { Cache } from '../src/Cache';
+
+describe('Cache', () => {
+	
+});
