58fc29d9125e9a1e1b9db48812b27fee8360ccf6
Michael Grenier <michael.grenier@live.com>
2018-09-12 13:11:33 -0400

    Fix lstree stream implementation

--- packages/konstellio/fs/src/utils/lstree.ts
+++ packages/konstellio/fs/src/utils/lstree.ts
@@ -2,50 +2,51 @@ import { FileSystem, Stats } from "../FileSystem";
 import { Readable } from "stream";
 
 export function lstree(fs: FileSystem, path: string) {
-	const directories = [path];
+	const queue: [string, Stats][] = [];
 	let first = true;
-	let abort = false;
 	return new Readable({
 		objectMode: true,
 		highWaterMark: 1,
 		read(size) {
-			if (abort) {
-				return this.push(null);
-			}
 			if (first) {
 				fs.stat(path)
 				.then(stat => {
 					first = false;
 					if (stat.isFile) {
-						abort = true;
+						this.push([path, stat]);
+					} else {
+						queue.push([path, stat]);
+						processQueue(this);
 					}
-					this.push([path, stat]);
 				})
 				.catch(err => {
-					abort = true;
 					this.emit('error', err);
 				});
 			}
-			else if (directories.length === 0) {
+			else if (queue.length === 0) {
 				this.push(null);
 			}
 			else {
-				const path = directories.shift()!;
-				fs.readDirectory(path, true)
-				.then(entries => {
-					for (const entry of entries) {
-						entry[0] = `${path}/${entry[0]}`;
-						if (!entry[1].isFile) {
-							directories.push(entry[0]);
-						}
-						this.push(entry);
-					}
-				})
-				.catch(err => {
-					abort = true;
-					this.emit('error', err);
-				});
+				processQueue(this);
 			}
 		}
 	});
+	function processQueue(stream: Readable) {
+		const [path, stat] = queue.shift()!;
+		if (stat.isFile) {
+			stream.push([path, stat]);
+		} else {
+			fs.readDirectory(path, true)
+			.then(entries => {
+				for (const entry of entries) {
+					entry[0] = `${path}/${entry[0]}`;
+					queue.push(entry);
+				}
+				stream.push([path, stat]);
+			})
+			.catch(err => {
+				stream.emit('error', err);
+			});
+		}
+	}
 }
