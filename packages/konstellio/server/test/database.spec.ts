3c4bbc2c6031875faab4655aa00e30bfbf0ef183
Michael Grenier <michael.grenier@live.com>
2018-09-19 18:54:56 -0400

    Added more test to database

--- packages/konstellio/server/test/database.spec.ts
+++ packages/konstellio/server/test/database.spec.ts
@@ -1,15 +1,14 @@
 import "mocha";
 import { expect, should } from "chai";
-import { createSchemaFromDefinitions, createSchemaFromDatabase, computeSchemaDiff } from "../src/utilities/migration";
+import { createSchemaFromDefinitions, createSchemaFromDatabase, computeSchemaDiff, executeSchemaDiff } from "../src/utilities/migration";
 import { parse } from "graphql";
 import { DatabaseSQLite } from "@konstellio/db-sqlite";
-import { ColumnType } from "@konstellio/db";
+import { ColumnType, Column, q } from "@konstellio/db";
 should();
 
 describe('Database', () => {
 
 	describe('calc schema diff', () => {
-
 		it('simple collection table', async () => {
 			const db = await new DatabaseSQLite({ filename: ':memory:' }).connect();
 			const locales = { en: 'English' };
@@ -25,22 +24,7 @@ describe('Database', () => {
 			const dbSchema = await createSchemaFromDatabase(db, locales);
 			const schemaDiffs = computeSchemaDiff(dbSchema, schema, db.compareTypes);
 
-			expect(schemaDiffs).to.eql([
-				{
-					action: 'add_collection',
-					collection: {
-						handle: 'CollectionA',
-						fields: [
-							{ handle: 'id', size: -1, type: ColumnType.Text },
-							{ handle: 'title', size: -1, type: ColumnType.Text }
-						],
-						indexes: []
-					},
-					sourceSchema: {
-						collections: []
-					}
-				}
-			]);
+			expect(JSON.stringify(schemaDiffs)).to.eql('[{"action":"add_collection","collection":{"handle":"CollectionA","indexes":[],"fields":[{"handle":"id","type":"text","size":-1},{"handle":"title","type":"text","size":-1}]},"sourceSchema":{"collections":[]}}]');
 		});
 
 		it('localized collection table', async () => {
@@ -58,23 +42,7 @@ describe('Database', () => {
 			const dbSchema = await createSchemaFromDatabase(db, locales);
 			const schemaDiffs = computeSchemaDiff(dbSchema, schema, db.compareTypes);
 
-			expect(schemaDiffs).to.eql([
-				{
-					action: 'add_collection',
-					collection: {
-						handle: 'CollectionA',
-						fields: [
-							{ handle: 'id', size: -1, type: ColumnType.Text },
-							{ handle: 'title__en', size: -1, type: ColumnType.Text },
-							{ handle: 'title__fr', size: -1, type: ColumnType.Text }
-						],
-						indexes: []
-					},
-					sourceSchema: {
-						collections: []
-					}
-				}
-			]);
+			expect(JSON.stringify(schemaDiffs)).to.eql('[{"action":"add_collection","collection":{"handle":"CollectionA","indexes":[],"fields":[{"handle":"id","type":"text","size":-1},{"handle":"title__en","type":"text","size":-1},{"handle":"title__fr","type":"text","size":-1}]},"sourceSchema":{"collections":[]}}]');
 		});
 
 		it('indexed collection table', async () => {
@@ -83,7 +51,7 @@ describe('Database', () => {
 			const definitions = parse(`
 				type CollectionA
 				@collection
-				@index(handle: "CollectionA_Primary", type: "primary", fields: [{ field: "id", direction: "asc" }])
+				@index(handle: "CollectionA_id", type: "primary", fields: [{ field: "id", direction: "asc" }])
 				{
 					id: ID!
 					title: String!
@@ -103,7 +71,7 @@ describe('Database', () => {
 							{ handle: 'title', size: -1, type: ColumnType.Text }
 						],
 						indexes: [
-							{ handle: 'CollectionA_Primary', type: 'primary', fields: [{ field: 'id', direction: 'asc' }] }
+							{ handle: 'CollectionA_id', type: 'primary', fields: [{ field: 'id', direction: 'asc' }] }
 						]
 					},
 					sourceSchema: {
@@ -134,91 +102,102 @@ describe('Database', () => {
 			const dbSchema = await createSchemaFromDatabase(db, locales);
 			const schemaDiffs = computeSchemaDiff(dbSchema, schema, db.compareTypes);
 
-			expect(schemaDiffs).to.eql([
-				{
-					action: 'add_field',
-					collection: {
-						handle: 'CollectionA',
-						fields: [
-							{ handle: 'id', size: -1, type: ColumnType.Text },
-							{ handle: 'title__en', size: -1, type: ColumnType.Text },
-							{ handle: 'title__fr', size: -1, type: ColumnType.Text }
-						],
-						indexes: [
-							{ handle: 'CollectionA_id', type: 'primary', fields: [{ field: 'id', direction: 'asc' }] }
-						]
-					},
-					field: {
-						handle: 'title__en',
-						size: -1,
-						type: ColumnType.Text
-					},
-					sourceCollection: {
-						handle: 'CollectionA',
-						fields: [
-							{ handle: 'id', size: -1, type: ColumnType.Text },
-							{ handle: 'title', size: -1, type: ColumnType.Text },
-						],
-						indexes: [
-							{ handle: 'CollectionA_id', type: 'primary', fields: [{ field: 'id', direction: 'asc' }] }
-						]
-					}
-				},
-				{
-					action: 'add_field',
-					collection: {
-						handle: 'CollectionA',
-						fields: [
-							{ handle: 'id', size: -1, type: ColumnType.Text },
-							{ handle: 'title__en', size: -1, type: ColumnType.Text },
-							{ handle: 'title__fr', size: -1, type: ColumnType.Text }
-						],
-						indexes: [
-							{ handle: 'CollectionA_id', type: 'primary', fields: [{ field: 'id', direction: 'asc' }] }
-						]
-					},
-					field: {
-						handle: 'title__fr',
-						size: -1,
-						type: ColumnType.Text
-					},
-					sourceCollection: {
-						handle: 'CollectionA',
-						fields: [
-							{ handle: 'id', size: -1, type: ColumnType.Text },
-							{ handle: 'title', size: -1, type: ColumnType.Text },
-						],
-						indexes: [
-							{ handle: 'CollectionA_id', type: 'primary', fields: [{ field: 'id', direction: 'asc' }] }
-						]
-					}
-				},
-				{
-					action: 'drop_field',
-					collection: {
-						handle: 'CollectionA',
-						fields: [
-							{ handle: 'id', size: -1, type: ColumnType.Text },
-							{ handle: 'title__en', size: -1, type: ColumnType.Text },
-							{ handle: 'title__fr', size: -1, type: ColumnType.Text }
-						],
-						indexes: [
-							{ handle: 'CollectionA_id', type: 'primary', fields: [{ field: 'id', direction: 'asc' }] }
-						]
-					},
-					field: {
-						handle: 'title',
-						size: -1,
-						type: ColumnType.Text
-					}
-				}
-			]);
+			expect(JSON.stringify(schemaDiffs)).to.eql('[{"sourceCollection":{"handle":"CollectionA","indexes":[{"handle":"CollectionA_id","type":"primary","fields":[{"field":"id","direction":"asc"}]}],"fields":[{"handle":"id","type":"text","size":-1},{"handle":"title","type":"text","size":-1}]},"action":"add_field","collection":{"handle":"CollectionA","indexes":[{"handle":"CollectionA_id","type":"primary","fields":[{"field":"id","direction":"asc"}]}],"fields":[{"handle":"id","type":"text","size":-1},{"handle":"title__en","type":"text","size":-1},{"handle":"title__fr","type":"text","size":-1}]},"field":{"handle":"title__en","type":"text","size":-1}},{"sourceCollection":{"handle":"CollectionA","indexes":[{"handle":"CollectionA_id","type":"primary","fields":[{"field":"id","direction":"asc"}]}],"fields":[{"handle":"id","type":"text","size":-1},{"handle":"title","type":"text","size":-1}]},"action":"add_field","collection":{"handle":"CollectionA","indexes":[{"handle":"CollectionA_id","type":"primary","fields":[{"field":"id","direction":"asc"}]}],"fields":[{"handle":"id","type":"text","size":-1},{"handle":"title__en","type":"text","size":-1},{"handle":"title__fr","type":"text","size":-1}]},"field":{"handle":"title__fr","type":"text","size":-1}},{"action":"drop_field","collection":{"handle":"CollectionA","indexes":[{"handle":"CollectionA_id","type":"primary","fields":[{"field":"id","direction":"asc"}]}],"fields":[{"handle":"id","type":"text","size":-1},{"handle":"title__en","type":"text","size":-1},{"handle":"title__fr","type":"text","size":-1}]},"field":{"handle":"title","type":"text","size":-1}}]');
 		});
-
 	});
 
-	xdescribe('execute diff', () => {
+	describe('execute diff', () => {
+		it('simple collection table', async () => {
+			const db = await new DatabaseSQLite({ filename: ':memory:' }).connect();
+			const locales = { en: 'English' };
+			const definitions = parse(`
+				type CollectionA
+				@collection
+				{
+					id: ID!
+					title: String!
+				}
+			`);
+			const schema = await createSchemaFromDefinitions(definitions, locales, false);
+			const dbSchema = await createSchemaFromDatabase(db, locales);
+			const schemaDiffs = computeSchemaDiff(dbSchema, schema, db.compareTypes);
 
+			await executeSchemaDiff(schemaDiffs, db);
+
+			const desc = await db.execute(q.describeCollection('CollectionA'));
+
+			expect(JSON.stringify(desc)).to.eql('{"collection":{"name":"CollectionA"},"columns":[{"name":"id","type":"text","size":-1,"defaultValue":null,"autoIncrement":false},{"name":"title","type":"text","size":-1,"defaultValue":null,"autoIncrement":false}],"indexes":[]}');
+		});
+
+		it('localized collection table', async () => {
+			const db = await new DatabaseSQLite({ filename: ':memory:' }).connect();
+			const locales = { en: 'English', fr: 'French' };
+			const definitions = parse(`
+				type CollectionA
+				@collection
+				{
+					id: ID!
+					title: String! @localized
+				}
+			`);
+			const schema = await createSchemaFromDefinitions(definitions, locales, false);
+			const dbSchema = await createSchemaFromDatabase(db, locales);
+			const schemaDiffs = computeSchemaDiff(dbSchema, schema, db.compareTypes);
+
+			await executeSchemaDiff(schemaDiffs, db);
+
+			const desc = await db.execute(q.describeCollection('CollectionA'));
+			expect(JSON.stringify(desc)).to.eql('{"collection":{"name":"CollectionA"},"columns":[{"name":"id","type":"text","size":-1,"defaultValue":null,"autoIncrement":false},{"name":"title__en","type":"text","size":-1,"defaultValue":null,"autoIncrement":false},{"name":"title__fr","type":"text","size":-1,"defaultValue":null,"autoIncrement":false}],"indexes":[]}');
+		});
+
+		it('indexed collection table', async () => {
+			const db = await new DatabaseSQLite({ filename: ':memory:' }).connect();
+			const locales = { en: 'English' };
+			const definitions = parse(`
+				type CollectionA
+				@collection
+				@index(handle: "CollectionA_id", type: "primary", fields: [{ field: "id", direction: "asc" }])
+				{
+					id: ID!
+					title: String!
+				}
+			`);
+			const schema = await createSchemaFromDefinitions(definitions, locales, false);
+			const dbSchema = await createSchemaFromDatabase(db, locales);
+			const schemaDiffs = computeSchemaDiff(dbSchema, schema, db.compareTypes);
+
+			await executeSchemaDiff(schemaDiffs, db);
+
+			const desc = await db.execute(q.describeCollection('CollectionA'));
+			expect(JSON.stringify(desc)).to.eql('{"collection":{"name":"CollectionA"},"columns":[{"name":"id","type":"text","size":-1,"defaultValue":null,"autoIncrement":false},{"name":"title","type":"text","size":-1,"defaultValue":null,"autoIncrement":false}],"indexes":[{"name":"CollectionA_id","type":"primary","columns":[{"field":{"name":"id"},"direction":"asc"}]}]}');
+		});
+
+		it('simple collection table from existing table', async () => {
+			const db = await new DatabaseSQLite({ filename: ':memory:' }).connect();
+
+			const trnx = await db.transaction();
+			trnx.execute('CREATE TABLE CollectionA (id TEXT PRIMARY KEY, title TEXT)');
+			await trnx.commit();
+
+			const locales = { en: 'English', fr: 'French' };
+			const definitions = parse(`
+				type CollectionA
+				@collection
+				@index(handle: "CollectionA_id", type: "primary", fields: [{ field: "id", direction: "asc" }])
+				{
+					id: ID!
+					title: String! @localized
+				}
+			`);
+			const schema = await createSchemaFromDefinitions(definitions, locales, false);
+			const dbSchema = await createSchemaFromDatabase(db, locales);
+			const schemaDiffs = computeSchemaDiff(dbSchema, schema, db.compareTypes);
+
+			await executeSchemaDiff(schemaDiffs, db);
+
+			const desc = await db.execute(q.describeCollection('CollectionA'));
+			expect(JSON.stringify(desc)).to.eql('{"collection":{"name":"CollectionA"},"columns":[{"name":"id","type":"text","size":-1,"defaultValue":null,"autoIncrement":false},{"name":"title__en","type":"text","size":-1,"defaultValue":null,"autoIncrement":false},{"name":"title__fr","type":"text","size":-1,"defaultValue":null,"autoIncrement":false}],"indexes":[]}');
+		});
 	});
 
 });
