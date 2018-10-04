3cb58f646e924fe21929e65648d7951675a606e9
Michael Grenier <michael.grenier@live.com>
2018-10-04 19:52:42 -0400

    Added MessageQueue.subscribeIterator method

--- packages/konstellio/mq/src/MessageQueue.ts
+++ packages/konstellio/mq/src/MessageQueue.ts
@@ -12,8 +12,10 @@ export abstract class MessageQueue {
 	abstract subscribe(name: string, listener: SubscribListener): Promise<Disposable>;
 	abstract subscribe(name: string, topic: string, listener: SubscribListener): Promise<Disposable>;
 
-	async *subscribeIterator(name: string): AsyncIterator<Message> {
-		return new AsyncIteratorSubscription(this, name);
+	subscribeIterator<T>(name: string): AsyncIteratorSubscription<T>;
+	subscribeIterator<T>(name: string, topic: string): AsyncIteratorSubscription<T>;
+	subscribeIterator<T>(name: string, topic?: string): AsyncIteratorSubscription<T> {
+		return new AsyncIteratorSubscription<T>(this, name, topic);
 	}
 
 	abstract send(name: string, task: Payload): Promise<void>;
@@ -33,21 +35,28 @@ export interface Message {
 export type SubscribListener = (message: Message) => void;
 export type ConsumeListener = (task: Message) => void | Message | Promise<Message>;
 
-export class AsyncIteratorSubscription implements AsyncIterator<Message>, IDisposableAsync {
+export class AsyncIteratorSubscription<T> implements AsyncIterator<T>, IDisposableAsync {
 	private disposed: boolean;
 
-	protected messagePool: Pool<Message>;
+	protected messagePool: Pool<T>;
 	protected subscription: Promise<Disposable>;
 
 	constructor(
 		protected mq: MessageQueue,
-		protected name: string
+		protected name: string,
+		protected topic?: string
 	) {
 		this.disposed = false;
-		this.messagePool = new Pool<Message>();
-		this.subscription = this.mq.subscribe(this.name, (msg) => {
-			this.messagePool.release(msg);
-		});
+		this.messagePool = new Pool();
+		if (this.topic) {
+			this.subscription = this.mq.subscribe(this.name, this.topic, (msg) => {
+				this.messagePool.release(msg as any);
+			});
+		} else {
+			this.subscription = this.mq.subscribe(this.name, (msg) => {
+				this.messagePool.release(msg as any);
+			});
+		}
 	}
 
 	isDisposed(): boolean {
@@ -55,27 +64,38 @@ export class AsyncIteratorSubscription implements AsyncIterator<Message>, IDispo
 	}
 
 	async disposeAsync(): Promise<void> {
-		const disposable = await this.subscription;
-		disposable.dispose();
-		this.disposed = true;
+		if (!this.disposed) {
+			const disposable = await this.subscription;
+			disposable.dispose();
+			this.disposed = true;
+			(this as any).mq = undefined;
+			(this as any).messagePool = undefined;
+			(this as any).subscription = undefined;
+		}
 	}
 
-	async next(): Promise<IteratorResult<Message>> {
+	async next(): Promise<IteratorResult<T>> {
+		if (this.disposed) {
+			throw new Error(`This AsyncIteratorSubscription is disposed.`);
+		}
 		const msg = await this.messagePool.acquires();
 		return { value: msg, done: false };
 	}
 
-	async return(): Promise<IteratorResult<Message>> {
+	async return(): Promise<IteratorResult<T>> {
+		if (this.disposed) {
+			throw new Error(`This AsyncIteratorSubscription is disposed.`);
+		}
 		await this.disposeAsync();
 		return { value: undefined!, done: true };
 	}
 
-	async throw(error: Error): Promise<IteratorResult<Message>> {
+	async throw(error: Error): Promise<IteratorResult<T>> {
 		await this.disposeAsync();
 		return Promise.reject(error);
 	}
 
-	[Symbol.asyncIterator]() {
+	[Symbol.asyncIterator](): AsyncIterator<T> {
 		return this;
 	}
 }
--- packages/konstellio/mq/test/MessageQueue.spec.ts
+++ packages/konstellio/mq/test/MessageQueue.spec.ts
@@ -1,9 +1,33 @@
 import 'mocha';
-import { use, should } from 'chai';
+import { use, should, expect } from 'chai';
 use(require("chai-as-promised"));
 should();
-import { MessageQueue } from '../src/MessageQueue';
+import { MessageQueueMemory } from './MessageQueueMemory';
 
 describe('MessageQueue', () => {
 	
+	it('subscribeIterator', async () => {
+
+		const mq = new MessageQueueMemory();
+
+		let i = 0;
+		setInterval(() => {
+			mq.publish('test', { counter: ++i });
+		}, 500);
+
+		const iter = mq.subscribeIterator<{ counter: number }>('test');
+
+		const a = await iter.next();
+		expect(a.value.counter).to.equal(1);
+		expect(a.done).to.equal(false);
+
+		const b = await iter.next();
+		expect(b.value.counter).to.equal(2);
+		expect(b.done).to.equal(false);
+
+		const c = await iter.next();
+		expect(c.value.counter).to.equal(3);
+		expect(c.done).to.equal(false);
+	});
+
 });
